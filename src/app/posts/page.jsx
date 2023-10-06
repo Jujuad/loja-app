9d8d7be0b42fbc9d7d05f6b334fb4731ace2fbd6
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-06 20:58:41 -0300

    add de posts + imgs

---
+++ src/app/posts/page.jsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export default function MeuPrimeiroPost(){
+
+    return(
+        <>
+            <div>
+                    <h1>Meu Primeiro POST</h1>
+                    <div>
+                        <p><Link href="/">Voltar</Link></p>
+                    </div>
+            </div>
+        
+        </>
+    )
+
+}
